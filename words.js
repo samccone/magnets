f4ac2da05c59688e2c11bc7338fa406ba6bef8ba
Sam Saccone <sam@mojotech.com>
2012-05-24 19:20:31 -0400

    add persistence for when the server goes down

--- words.js
+++ words.js
@@ -1,5 +1,20 @@
-var _ = require('underscore');
-var words = {};
+var _       = require('underscore');
+var fs      = require('fs');
+var words   = loadWords();
+var canSave = true;
+
+
+function loadWords() {
+  fs.readFile('savedWords.txt', function(err, data){
+    if(err) {
+      console.log("Error loading words -- " + err);
+      words = {}
+    } else {
+      words = JSON.parse(data)
+    }
+  });
+  return {};
+}
 
 function generatePosition() {
   return {
@@ -8,11 +23,24 @@ function generatePosition() {
   };
 }
 
+function saveWords(force) {
+  if(canSave || force) {
+    canSave = false;
+    setTimeout(function(){ canSave = true }, 1000);
+    fs.writeFile('savedWords.txt', JSON.stringify(words), function(err){
+      if(err){
+        console.log("Error Saving Words");
+      }
+    });
+  }
+}
+
 exports.words = function() {
   return words;
 }
 
 exports.updatePosition = function(word) {
+  saveWords();
   words[word.word].position = word.position;
 }
 
@@ -22,5 +50,6 @@ exports.addWord = function(word) {
     'position' : generatePosition()
   };
   words[word] = newWord;
+  saveWords(true);
   return newWord;
 }
